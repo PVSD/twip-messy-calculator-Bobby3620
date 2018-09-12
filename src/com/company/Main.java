6004cdba950def88279b3d6b2941c9a6a4072ab0
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-09-11 20:02:26 -0400

    ayyyyyyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,78 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Where the scanner is created
+        Scanner kbInput = new Scanner(System.in);
+        Scanner kbOnce = new Scanner(System.in);
+        Scanner kbTwice = new Scanner(System.in);
+
+
+        //Where the user chooses
+        System.out.println("Please choose to either add, subtract, multiply or divide");
+        String kbOption = kbInput.nextLine();
+        System.out.println("You selected: " + kbOption.toLowerCase());
+
+        //If user choose add
+        if(kbOption.equals("add")){
+            System.out.println("Please enter a number");
+            int a = (int)kbInput.nextDouble();
+            System.out.println("Please enter another number");
+            int b = (int)kbInput.nextDouble();
+            int ab = a + b;
+            int togetherAdd = (int)Math.sqrt(ab);
+            System.out.println("This is your answer: " + togetherAdd + "." + " Are you satisfied?");
+            String kbAdd = kbOnce.nextLine();
+            System.out.println("You selected: " + kbAdd.toLowerCase());
+
+            //If user is happy
+            if(kbAdd.equals("yes")) {
+                System.out.println("Cool");
+            }else {
+                System.out.println("Can you guess the operation performed?");
+                String sqrt = kbTwice.nextLine();
+                System.out.println("You choose: " + sqrt.toLowerCase());
+
+                //Where the user selects the operation
+                if(sqrt.equals("square root")){
+                    System.out.println("Correct! Goodbye");
+                }else {
+                    System.out.println("Try again");
+                }
+            }
+        }
+
+        //If user choose subtract
+        if(kbOption.equals("subtract")){
+            System.out.println("Please enter a number");
+            int a = (int)kbInput.nextDouble();
+            System.out.println("Please enter another number");
+            int b = (int)kbInput.nextDouble();
+            int ab = a - b;
+            int togetherSubtract = Math.abs(ab);
+            System.out.println("This is your answer: " + togetherSubtract + "." + " Are you satisfied?");
+            String kbSubtract = kbOnce.nextLine();
+            System.out.println("You selected: " + kbSubtract.toLowerCase());
+
+            //If user is happy
+            if(kbSubtract.equals("yes")) {
+                System.out.println("Cool");
+            }else {
+                System.out.println("Can you guess the operation performed?");
+                String abs = kbTwice.nextLine();
+                System.out.println("You choose: " + abs.toLowerCase());
+
+                //Where the user selects the operation
+                if(abs.equals("absolute")){
+                    System.out.println("Correct! Goodbye");
+                }else {
+                    System.out.println("Try again");
+                }
+            }
+        }
+
     }
 }
